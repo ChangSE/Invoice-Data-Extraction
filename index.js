ea5adb31fdcd8275dccba410db5725c7c61c61fb
Chang-Liu <c392liu@gmail.com>
2020-10-04 20:26:36 -0400

    remove redundant code

--- index.js
+++ index.js
@@ -1,29 +1,14 @@
 const express = require('express');
-const multer  = require('multer');
 const mongoose = require('mongoose');
-const moment = require("moment");
-const pdfUtil = require('pdf-to-text');
 const bodyParser = require('body-parser');
 const keys = require('./config/keys');
 require('./models/Invoice');
 
 mongoose.connect(keys.mongoURI);
 
-//const Invoice = mongoose.model('invoices');
-
 const app = express();
 const port = 3000;
 
-// var storage = multer.diskStorage({
-//   destination: function (req, file, cb) {
-//     cb(null, 'invoices/')
-//   },
-//   filename: function (req, file, cb) {
-//     cb(null, file.originalname)
-//   }
-// })
-//
-// const upload = multer({ storage: storage });
 
 app.use(bodyParser.json());
 require('./routes/fetchInvoice')(app);
